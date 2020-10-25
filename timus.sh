6f9d1b3ec0e18d63e6c186637ae6db4c0ba11aec
vnikme <accs@vnik.me>
2020-10-25 03:47:11 -0400

    Atomic fetch for timus data.

--- timus.sh
+++ timus.sh
@@ -1,1 +1,2 @@
-python3 timus.py > /home/vnik/work/vnikme/frontend/vnikme/timus.json
+python3 timus.py > /home/vnik/work/vnikme/frontend/vnikme/timus.json.tmp
+mv /home/vnik/work/vnikme/frontend/vnikme/timus.json.tmp /home/vnik/work/vnikme/frontend/vnikme/timus.json
