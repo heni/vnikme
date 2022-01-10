1ce14652a56de3d0bcf79279b6be24831c69f051
vnikme <accs@vnik.me>
2022-01-10 17:13:27 -0500

    Update covid repo path.

--- fetch_covid.sh
+++ fetch_covid.sh
@@ -1,1 +1,1 @@
-curl "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.json" > /home/vnik/work/vnikme/frontend/vnikme/covid.json
+curl "https://covid.ourworldindata.org/data/owid-covid-data.json" > /home/vnik/work/vnikme/frontend/vnikme/covid.json
